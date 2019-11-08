54a02ab309537b5502a88da3da702d952d66b9ca
git-pusher <rachel.palomeque@gmail.com>
2019-11-08 13:22:33 -0600

    Etapa cuatro

--- Program.cs
+++ Program.cs
@@ -28,38 +28,45 @@ namespace Etapa1
             var otraColecion = new List<Curso>(){
                               new Curso() {Nombre = "401", Jornada = TiposJornada.Mañana},
                               new Curso() {Nombre = "501", Jornada = TiposJornada.Mañana},
+                              new Curso() {Nombre = "501", Jornada = TiposJornada.Tarde},
                               new Curso() {Nombre = "502", Jornada = TiposJornada.Tarde}
             };
 
-            //Creando un curso temporal 
+            //Creando un curso temporal para eliminarlo con Remove()
             Curso tmp = new Curso {Nombre = "101-Vacacional", Jornada = TiposJornada.Noche};
 
             //Métodos para adicionar
-            escuela.Cursos.AddRange(otraColecion);      
+            escuela.Cursos.AddRange(otraColecion);  
             escuela.Cursos.Add(tmp);
+            
             ImprimirCursosEscuela(escuela);
-            //WriteLine("Curso.Hash: " + tmp.GetHashCode());
-            // Predicate<Curso> miAlgoritmo;
-            // escuela.Cursos.RemoveAll(miAlgoritmo);
-            //escuela.Cursos.Remove(tmp);
+            WriteLine("Curso.Hash: " + tmp.GetHashCode());
+
+            //remover el curso temporal(un curso en específico)
+            escuela.Cursos.Remove(tmp);
+
+            //eliminar usando predicado
+            //Predicate<Curso> miAlgoritmo = Predicado;
+            //escuela.Cursos.RemoveAll( Predicado);
+            escuela.Cursos.RemoveAll( delegate (Curso cur )
+                                    {
+                                        return cur.Nombre == "301";
+                                    });
+            //expresiones lambda, el resultado es igual al delegado, pero más compacto
+            escuela.Cursos.RemoveAll(( cur ) => cur.Nombre == "501" && cur.Jornada == TiposJornada.Mañana);
+            
             WriteLine("==================================");
             ImprimirCursosEscuela(escuela);
 
             //Eliminar todo
             //otraColecion.Clear(); 
             //otraColecion.Remove(); 
-            
-
-
-            ////////////////////////////////////////////////////////////////////////////////////
-            //IMPRESIÓN FINAL
-
-
         }
 
-        // private static bool miAlgoritmo(Curso curobj)
+        //predicado -- ya no uso el predicado, uso delegate
+        // private static bool Predicado (Curso curobj)
         // {
-        //     return curobj.Nombre == " 301";
+        //     return curobj.Nombre == "301";
         // }
 
         private static void ImprimirCursosEscuela(Escuela escuela)
