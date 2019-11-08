f8edfc61e918671165db4feb2edf5fd845af1033
git-pusher <rachel.palomeque@gmail.com>
2019-11-08 11:48:05 -0600

    Primera versión del proyecto

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        string nombre;
+        public string Nombre {
+            get{ return "Copia: " + nombre; }
+            set{nombre = value.ToUpper(); } 
+        }
+        public int AñoDeCreacion { get;set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        public TiposEscuela TiposEscuela { get; set; }
+        public List<Curso> Cursos { get; set; }
+
+
+        
+
+        //CONSTRUCTOR -- Igualación por tuplas 
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año);
+        public Escuela(string nombre, 
+                       int año, 
+                       TiposEscuela tipo, 
+                       string pais="", 
+                       string ciudad="") 
+        {
+            (Nombre, AñoDeCreacion) = (nombre, año);
+            Pais = pais;
+            Ciudad = ciudad;
+        }
+
+        public override string ToString() {
+            return $"Nombre: {Nombre}, \nTipo: {TiposEscuela}, \nPais: {Pais}, \nCiudad: {Ciudad}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreEscuela.Entidades;
+using static System.Console;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
+            pais: "Colombia", ciudad: "Bogotá");
+
+            //LISTA
+            escuela.Cursos = new List<Curso>(){
+                              new Curso() {Nombre = "101", Jornada = TiposJornada.Mañana},
+                              new Curso() {Nombre = "201", Jornada = TiposJornada.Mañana},
+                              new Curso() {Nombre = "301", Jornada = TiposJornada.Mañana}
+            };
+           
+
+            //En las listas puedo agregar nuevos elementos
+            escuela.Cursos.Add( new Curso {Nombre = "102", Jornada = TiposJornada.Tarde});            
+            escuela.Cursos.Add( new Curso {Nombre = "202", Jornada = TiposJornada.Tarde});    
+
+            //creo nueva lista
+            var otraColecion = new List<Curso>(){
+                              new Curso() {Nombre = "401", Jornada = TiposJornada.Mañana},
+                              new Curso() {Nombre = "501", Jornada = TiposJornada.Mañana},
+                              new Curso() {Nombre = "502", Jornada = TiposJornada.Tarde}
+            };
+
+            //Creando un curso temporal 
+            Curso tmp = new Curso {Nombre = "101-Vacacional", Jornada = TiposJornada.Noche};
+
+            //Métodos para adicionar
+            escuela.Cursos.AddRange(otraColecion);      
+            escuela.Cursos.Add(tmp);
+            ImprimirCursosEscuela(escuela);
+            //WriteLine("Curso.Hash: " + tmp.GetHashCode());
+            // Predicate<Curso> miAlgoritmo;
+            // escuela.Cursos.RemoveAll(miAlgoritmo);
+            //escuela.Cursos.Remove(tmp);
+            WriteLine("==================================");
+            ImprimirCursosEscuela(escuela);
+
+            //Eliminar todo
+            //otraColecion.Clear(); 
+            //otraColecion.Remove(); 
+            
+
+
+            ////////////////////////////////////////////////////////////////////////////////////
+            //IMPRESIÓN FINAL
+
+
+        }
+
+        // private static bool miAlgoritmo(Curso curobj)
+        // {
+        //     return curobj.Nombre == " 301";
+        // }
+
+        private static void ImprimirCursosEscuela(Escuela escuela)
+        {
+            WriteLine("=====================\nCursos de la escuela\n====================="); 
+            // if(escuela != null && escuela.Cursos != null) 
+            if(escuela?.Cursos != null) 
+            {               
+                foreach (var curso in escuela.Cursos)
+                {
+                    WriteLine($"Nombre: {curso.Nombre}, Id: {curso.UniqueId} ");
+                }
+            }
+        }        
+    }
+}
