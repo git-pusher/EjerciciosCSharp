7edb684dac2daaefab72306ad14b4c81b0268011
Rachel E. Palomeque <47130226+git-pusher@users.noreply.github.com>
2019-12-04 15:35:17 -0600

    Delete Asignatura.cs

--- Estapa 5/Entidades/Asignatura.cs
+++
@@ -1,8 +0,0 @@
-using System;
-namespace CoreEscuela.Entidades
-{
-    public class Asignatura : ObjetoEscuelaBase
-    {
-        //hace lo que hereda del padre, pero se llama Asignatura    
-    }
-}
