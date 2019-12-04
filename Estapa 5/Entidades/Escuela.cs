64355b9d0a302175cf4cf61aae21bce25c09eee3
git-pusher <rachel.palomeque@gmail.com>
2019-12-04 12:25:55 -0600

    Etapa 5

---
+++ Estapa 5/Entidades/Escuela.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace CoreEscuela.Entidades
+{
+    public class Escuela
+    {
+        public string UniqueId { get; private set; } = Guid.NewGuid().ToString();
+        string nombre;
+        public string Nombre {
+            get{ return "Copia: " + nombre; }
+            set{nombre = value.ToUpper(); } 
+        }
+        public int AñoDeCreacion { get;set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        public TiposEscuela TiposEscuela { get; set; }
+        public List<Curso> Cursos { get; set; }
+
+
+        
+
+        //CONSTRUCTOR -- Igualación por tuplas 
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año);
+        public Escuela(string nombre, 
+                       int año, 
+                       TiposEscuela tipo, 
+                       string pais="", 
+                       string ciudad="") 
+        {
+            (Nombre, AñoDeCreacion) = (nombre, año);
+            Pais = pais;
+            Ciudad = ciudad;
+        }
+
+        public override string ToString() {
+            return $"Nombre: {Nombre}, \nTipo: {TiposEscuela}, \nPais: {Pais}, \nCiudad: {Ciudad}";
+        }
+    }
+}
